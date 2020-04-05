3a8e80fe942ab6969b265b498909627bbaf8f57d
Thiago Lima <thiago.lima@brainiac.home>
2020-04-05 20:52:16 -0300

    feat: Integrate with Apache Spark
    It already extract some metrics for downloaded files

--- src/workers/__init__.py
+++ src/workers/__init__.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 import gzip
 import shutil
 import tempfile
@@ -10,7 +12,11 @@ import ftplib
 import requests
 
 from celery import Celery
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
+
+DATA_DIR = '/srv/data'
+
+sc = SparkContext(conf=SparkConf())
 
 app = Celery(__name__, broker=os.environ['REDIS_URL'])
 
@@ -21,13 +27,16 @@ def perform_file_operations(url):
   if archive.endswith('.gz'):
     archive = extract_gzip_file(archive)
 
-def download(url):
-  logging.info("Downloading file %s" % url)
+  get_stats_from_spark(archive)
 
+
+def download(url):
   _, ext = os.path.splitext(url)
 
   parsed_url = urllib.parse.urlparse(url)
-  destination = tempfile.mktemp(dir='/dev/shm', suffix=ext)
+  destination = tempfile.mktemp(dir=DATA_DIR, suffix=ext)
+
+  logging.info("Downloading file %s into %s" % (url, destination))
 
   if parsed_url.scheme == 'ftp':
     return download_from_ftp(parsed_url, destination)
@@ -58,10 +67,47 @@ def download_from_ftp(parsed_url, destination):
   return destination
 
 def extract_gzip_file(gzip_file_path):
-  destination = tempfile.mktemp(dir='/dev/shm')
+  logging.info("Extracting file %s" % gzip_file_path)
+
+  destination = tempfile.mktemp(dir=DATA_DIR)
 
   with gzip.open(gzip_file_path, 'r') as gzip_file:
     with open(destination, 'wb') as text_file:
       shutil.copyfileobj(gzip_file, text_file)
 
   return destination
+
+def get_stats_from_spark(text_file):
+  logging.info("Processing file %s into Spark" % text_file)
+
+  txt = sc.textFile(text_file)
+
+  bytes_regex = re.compile('(\d+)$')
+  status_regex = re.compile('(\d+)\s[\d|\-]+$')
+  host_regex = re.compile('^([\w+\.]+[\w+]{1,}[^\s])')
+
+  unique_hosts = txt \
+    .flatMap(lambda line: host_regex.findall(line)) \
+    .distinct() \
+    .count()
+
+  total_errors = txt \
+    .filter(lambda line: status_regex.findall(line) == ['404']) \
+    .count()
+
+  top_5_urls = None
+  qty_404_by_day = None
+
+  total_bytes = txt \
+    .filter(lambda line: bytes_regex.findall(line) != []) \
+    .map(lambda line: bytes_regex.findall(line)) \
+    .map(lambda nbytes: int(nbytes[0])) \
+    .sum()
+
+  logging.info(
+    'We have %d unique hosts, with a total of %d 404 errors, and %d bytes transferred' % (
+      unique_hosts,
+      total_errors,
+      total_bytes
+    )
+  )
