1245c1853632caa43b101f65a9afac08521c01f5
Thiago Lima <thiago.lima@brainiac.home>
2020-04-05 16:35:49 -0300

    feat: Implement workers module
    It already support download files

---
+++ src/workers/__init__.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+import tempfile
+
+import logging
+
+import urllib
+import ftplib
+import requests
+
+from celery import Celery
+from pyspark import SparkContext
+
+app = Celery(__name__, broker=os.environ['REDIS_URL'])
+
+@app.task
+def perform_file_operations(url):
+  download(url)
+
+def download(url):
+  logging.info("Downloading file %s" % url)
+
+  _, ext = os.path.splitext(url)
+
+  parsed_url = urllib.parse.urlparse(url)
+  destination = tempfile.mktemp(dir='/dev/shm', suffix=ext)
+
+  if parsed_url.scheme == 'ftp':
+    return download_from_ftp(parsed_url, destination)
+  elif parsed_url.scheme == 'http':
+    return download_from_http(parsed_url, destination)
+  else:
+    logging.error("Unsupported URL scheme %s" % parsed_url.scheme)
+
+def download_from_http(parsed_url, destination):
+  filecontent = requests.get(parsed_url.geturl(), stream=True)
+
+  with open(destination, 'wb') as f:
+    for chunck in response.iter_content(chunck_size=1024):
+      f.write(chunck)
+
+  return destination
+
+def download_from_ftp(parsed_url, destination):
+  username = 'anonymous'
+
+  if parsed_url.username is not None:
+    username = parsed_url.username
+
+  with open(destination, 'wb') as f:
+    ftp = ftplib.FTP(parsed_url.hostname, username, parsed_url.password)
+    ftp.retrbinary("RETR " + parsed_url.path, f.write)
+
+  return destination
